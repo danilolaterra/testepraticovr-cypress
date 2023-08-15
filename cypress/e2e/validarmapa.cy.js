8735c4c81ed31c555a0572eb9aab5143e86e58fd
danilolaterra <danilo.laterra@gmail.com>
2023-08-15 14:35:05 -0300

    Atualizando proejto para novo caminho

--- cypress/e2e/validarmapa.cy.js
+++ cypress/e2e/validarmapa.cy.js
@@ -3,33 +3,15 @@
 
 describe ('inicial', ()=> {
     //Acessando a pagina Home do site VR - coloquei a BaseUrl no arquivo (cypress.json)
-    it ('acessar pagina inicial', ()=> {
+    it ('ACESSANDO PAGINA VR E VERIFICANDO SE O MAPA ESTA VISIVEL', ()=> {
         cy.visit('/')
-    })
-
-    //Aceitando os Termos de Privacidade da pagina
-    it ('Aceitar termos de Privacidade', ()=> {
-        cy.get('#lum_4028808471FC1D5D0171FE80340D6AC8_quickAcceptTerm').click();
-    })
-
-    //Localizando o botao "PRA VOCE" e clicando
-    it ('Navegar até a seção - pra voce', ()=> {
-        cy.get(':nth-child(3) > .vr-main-navigation__link').click();        
-    })
-    //Apos clicar no botao, estou validando se realmente fui direcionado para a pagina correta (/onde-aceita.htm)
-    it ('Validar se estou na pagina correta', ()=> {
-        cy.url().should(
-            'be.equal',
-            `${Cypress.config("baseUrl")}/onde-aceita.htm`
-          )
-    })
-
-    //Localizando o botao "ONDE USAR MEU CARTAO VR" e clicando
-    it ('Clicar no botão - Onde usar meu cartão VR', ()=> {
-        cy.get('.vr-button.vr-button--negative').click();
-
-        //Aqui eu estou validando se o MAPA está visivel após clicar no botão
+        //Aceitando os Termos de Privacidade da pagina
+        cy.get('#lum_4028808471FC1D5D0171FE80340D6AC8_quickAcceptTerm').click()
+        //Acessando menu SOU TRABALHADOR
+        cy.get(':nth-child(3) > .vr-main-navigation__link').click()
+        //Fazendo Scroll ate a localização do mapa
+        cy.contains('Rede credenciada').scrollIntoView()
+        //Verificando se o mapa esta visivel
         cy.get('#map').should('be.visible').wait(4000);
     })
-
 });
